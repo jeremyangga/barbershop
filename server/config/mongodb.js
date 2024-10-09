6dcc452a7d5705ae240dbe4181ee2c93f3536138
jeremyangga <62837186+jeremyangga@users.noreply.github.com>
2024-10-10 00:57:14 +0700

    Update

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = "mongodb+srv://mongo:jeje_mongo@cluster0.cuoltyf.mongodb.net/";
+const uri = ;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
